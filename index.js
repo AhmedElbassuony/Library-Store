666f4a86bee67530a32e7e34f04e5c060dd4aa1a
Abo_Tolba <122931774+AhmedElbassuony@users.noreply.github.com>
2025-07-30 23:42:35 +0300

    Intitalize project,env ,and create the database connection

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://127.0.0.1:${PORT}`);
+});
